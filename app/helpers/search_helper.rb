de82530199f1de7b889dfcf185cce13b9e0792d5
Alex McHale <alex@anticlever.com>
2012-03-17 23:15:11 -0500

    Refactor into helpers how the search screen is constructed.

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,29 @@
 module SearchHelper
+
+  def active_media_class id
+    last_media = session[:last_search][:media] if session[:last_search]
+    last_media ||= "all"
+
+    if last_media == id then "active" else "" end
+  end
+
+  def search_tab_for id, text
+    content_tag :li, :class => active_media_class(id) do
+      content_tag :a, "data-toggle" => "tab", "href" => "##{id}" do
+        text
+      end
+    end
+  end
+
+  def search_pane_for id, &b
+    content_tag :div, :class => "tab-pane #{active_media_class id}", :id => id do
+      form_tag search_path do
+        raw [
+          hidden_field_tag("search[media]", id),
+          content_tag(:div, &b)
+        ].join
+      end
+    end
+  end
+
 end
