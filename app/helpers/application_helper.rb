bc485e42b87d9078f5367a6264efe702a1ab6dc4
Alex McHale <alex@anticlever.com>
2012-03-19 10:11:35 -0500

    Graphical improvements.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def active_if page
+    c, a = page.split "#"
+    "active" if controller.controller_name == c && controller.action_name == a
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120318185721) do
+ActiveRecord::Schema.define(:version => 20120319141025) do
 
   create_table "records", :force => true do |t|
     t.integer  "retailer_id"
