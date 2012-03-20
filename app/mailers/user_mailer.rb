e4a9950b6a8969b87265f8078601503efae77f51
Alex McHale <alex@anticlever.com>
2012-03-20 12:21:32 -0500

    Improve the record_added email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,11 +3,11 @@ class UserMailer < ActionMailer::Base
   default from: "upcoming@anticlever.com"
 
   def record_added search, record
-    @search = search
-    @record = record
-    @user = @search.user
-    @style = @search.style.singularize
-    @subject = "New %s found on search `%s`: %s" % [ @style, @search.term, @record.name ]
+    @search  = search
+    @record  = record
+    @user    = @search.user
+    @style   = @search.style.singularize.downcase
+    @subject = "[Upcoming] New %s found: %s" % [ @style, @record.name ]
 
     mail to: @user.email, subject: @subject
   end
