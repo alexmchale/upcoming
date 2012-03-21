8724e4bd7f840e86a41ceb4151604f94f60beb56
Alex McHale <alex@anticlever.com>
2012-03-21 15:40:31 -0500

    Improve the logged-in experience. Save sessions for 10 years and redirect logged-in users to the searches page if they aren't clicking to the welcome page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
+    if current_user && request.referer.blank?
+      redirect_to searches_path
+    end
   end
 
   protected
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Upcoming::Application.config.session_store :cookie_store, key: '_upcoming_session'
+Upcoming::Application.config.session_store :cookie_store,
+  key: '_upcoming_session',
+  expire_after: 10.years
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
